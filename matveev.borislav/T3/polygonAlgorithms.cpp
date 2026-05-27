97b48f32bf1b73cbf2f6c61d4acb807a6b13b21e
L1sa620 <lisevbom@gmail.com>
2026-05-27 17:16:14 +0300

    Add frame generate

--- matveev.borislav/T3/polygonAlgorithms.cpp
+++ matveev.borislav/T3/polygonAlgorithms.cpp
@@ -5,6 +5,8 @@
 #include <iterator>
 #include <numeric>
 #include <vector>
+#include <functional>
+#include <stdexcept>
 
 namespace
 {
@@ -32,6 +34,31 @@ private:
   const matveev::Point& base_;
   std::vector< matveev::Point >::const_iterator current_;
 };
+}
+
+bool isLessX(const matveev::Point& lhs, const matveev::Point& rhs)
+{
+  return lhs.x < rhs.x;
+}
+
+bool isLessY(const matveev::Point& lhs, const matveev::Point& rhs)
+{
+  return lhs.y < rhs.y;
+}
+
+matveev::Point getLeftBottom(const matveev::Frame& frame)
+{
+  return frame.left_bottom;
+}
+
+matveev::Point getRightTop(const matveev::Frame& frame)
+{
+  return frame.right_top;
+}
+
+void copyPoints(const matveev::Polygon& polygon, std::vector< matveev::Point >& points)
+{
+  std::copy(polygon.points.begin(), polygon.points.end(), std::back_inserter(points));
 }
 
 double matveev::getTriangleArea(const Point& first, const Point& second, const Point& third)
@@ -63,4 +90,50 @@ double matveev::getPolygonArea(const Polygon& polygon)
   );
 
   return std::accumulate(areas.begin(), areas.end(), 0.0);
+}
+
+matveev::Frame matveev::getFrame(const Polygon& polygon)
+{
+  if (polygon.points.empty())
+  {
+    throw std::logic_error("empty polygon");
+  }
+
+  std::vector< Point >::const_iterator min_x = std::min_element(polygon.points.begin(), polygon.points.end(), isLessX);
+  std::vector< Point >::const_iterator max_x = std::max_element(polygon.points.begin(), polygon.points.end(), isLessX);
+  std::vector< Point >::const_iterator min_y = std::min_element(polygon.points.begin(), polygon.points.end(), isLessY);
+  std::vector< Point >::const_iterator max_y = std::max_element(polygon.points.begin(), polygon.points.end(), isLessY);
+
+  Frame frame = { Point(min_x->x, min_y->y), Point(max_x->x, max_y->y) };
+  return frame;
+}
+
+matveev::Frame matveev::getFrame(const std::vector< Polygon >& polygons)
+{
+  if (polygons.empty())
+  {
+    throw std::logic_error("empty polygons");
+  }
+
+  std::vector< Point > points;
+  std::for_each(polygons.begin(), polygons.end(), std::bind(copyPoints, std::placeholders::_1, std::ref(points)));
+
+  Polygon polygon;
+  polygon.points = points;
+
+  return getFrame(polygon);
+}
+
+bool matveev::isPointInFrame(const Frame& frame, const Point& point)
+{
+  return point.x >= frame.left_bottom.x
+    && point.x <= frame.right_top.x
+    && point.y >= frame.left_bottom.y
+    && point.y <= frame.right_top.y;
+}
+
+bool matveev::isPolygonInFrame(const Frame& frame, const Polygon& polygon)
+{
+  using namespace std::placeholders;
+  return std::all_of(polygon.points.begin(), polygon.points.end(), std::bind(isPointInFrame, frame, _1));
 }
--- matveev.borislav/T3/polygonAlgorithms.hpp
+++ matveev.borislav/T3/polygonAlgorithms.hpp
@@ -3,10 +3,23 @@
 
 #include "geometry.hpp"
 
+#include <vector>
+
 namespace matveev
 {
+struct Frame
+{
+  Point left_bottom;
+  Point right_top;
+};
+
 double getTriangleArea(const Point& first, const Point& second, const Point& third);
 double getPolygonArea(const Polygon& polygon);
+
+Frame getFrame(const Polygon& polygon);
+Frame getFrame(const std::vector< Polygon >& polygons);
+bool isPointInFrame(const Frame& frame, const Point& point);
+bool isPolygonInFrame(const Frame& frame, const Polygon& polygon);
 }
 
 #endif
