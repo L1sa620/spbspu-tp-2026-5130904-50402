2fa360c14690f034c970e5ff5510b7dfdc43a612
L1sa620 <lisevbom@gmail.com>
2026-06-25 00:39:43 +0300

    Fix cg 2

--- matveev.borislav/T1/commands.hpp
+++ matveev.borislav/T1/commands.hpp
@@ -1,5 +1,5 @@
-#ifndef COMMANDS_HPP
-#define COMMANDS_HPP
+#ifndef MATVEEV_COMMANDS_HPP
+#define MATVEEV_COMMANDS_HPP
 
 #include <iostream>
 #include <unordered_map>
@@ -10,18 +10,17 @@ namespace matveev
   using cmd_t = void(*)(std::istream&, std::ostream&, db_t&);
   using cmd_map_t = std::unordered_map< std::string, cmd_t >;
 
+  cmd_map_t initCommands();
+
   void create_note(std::istream&, std::ostream&, db_t&);
   void add_line(std::istream&, std::ostream&, db_t&);
   void show_note(std::istream&, std::ostream&, db_t&);
   void drop_note(std::istream&, std::ostream&, db_t&);
-
   void link_note(std::istream&, std::ostream&, db_t&);
   void mind_note(std::istream&, std::ostream&, db_t&);
   void halt_note(std::istream&, std::ostream&, db_t&);
-
   void expired_note(std::istream&, std::ostream&, db_t&);
   void refresh_note(std::istream&, std::ostream&, db_t&);
-
   void loop_note(std::istream&, std::ostream&, db_t&);
 }
 
