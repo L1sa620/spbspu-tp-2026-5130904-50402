b48c3f5ba0754ceeaaf182959e10071820c7a4c5
L1sa620 <lisevbom@gmail.com>
2026-06-25 00:28:01 +0300

    Fix comments

--- matveev.borislav/T1/main.cpp
+++ matveev.borislav/T1/main.cpp
@@ -1,38 +1,27 @@
 #include <iostream>
 #include <limits>
-#include <stdexcept>
 #include <string>
 #include "commands.hpp"
-
+ 
 int main()
 {
-  matveev::db_t db;
-  matveev::cmd_map_t cmds;
-  cmds["note"] = matveev::create_note;
-  cmds["line"] = matveev::add_line;
-  cmds["show"] = matveev::show_note;
-  cmds["drop"] = matveev::drop_note;
-  cmds["link"] = matveev::link_note;
-  cmds["mind"] = matveev::mind_note;
-  cmds["halt"] = matveev::halt_note;
-  cmds["expired"] = matveev::expired_note;
-  cmds["refresh"] = matveev::refresh_note;
-  cmds["loop"] = matveev::loop_note;
-
-  std::string cmd;
-  while (std::cin >> cmd)
-  {
-    try
-    {
-      cmds.at(cmd)(std::cin, std::cout, db);
-    }
-    catch (const std::logic_error&)
-    {
-      std::cout << "<INVALID COMMAND>\n";
-      std::cin.clear();
-      std::cin.ignore(std::numeric_limits< std::streamsize >::max(), '\n');
-    }
-  }
-
-  return 0;
+matveev::db_t db;
+auto cmds = matveev::initCommands();
+ 
+std::string cmd;
+while (std::cin >> cmd)
+{
+try
+{
+cmds.at(cmd)(std::cin, std::cout, db);
+}
+catch (const std::exception&)
+{
+std::cout << "<INVALID COMMAND>";
+std::cin.ignore(std::numeric_limits< std::streamsize >::max(), '\n');
+}
+std::cout << "\n";
+}
+ 
+return 0;
 }
